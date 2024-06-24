7c76543c91c69443c3bff57ec896e50574a7460a
Vitinho2 <vi.vitorsouza150@gmail.com>
2024-06-24 15:49:05 -0300

    modelo de vaga e estilização finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ export function App() {
 
   return (
     <>
-      <h1>hello world</h1>
+      <header></header>
+      <main>
+        <div className='job'>
+          <img src="" alt="foto" />
+          <div className="infoJob">
+            <div className="info">
+              <p className="nameCompany">Photosnap</p>
+              <div className="office">Senior Frontend Developer</div>
+              <div className="aboutJob">p</div>
+            </div>
+            <div className="requirements">
+              <div>FrontEnd</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
