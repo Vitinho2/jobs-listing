48f32115babc9200bd08e0160b2dfcf7eeb24d6e
Vitinho2 <vi.vitorsouza150@gmail.com>
2024-06-24 18:42:16 -0300

    Consumindo o arquivo data.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { fetchData } from '../public/services/dataService'
 import './App.css'
 
 export function App() {
+
+  const [objects, setObjects] = useState([])
+
+  useEffect(() => {
+    async function fetchMyData(){
+      const data = await fetchData()
+      setObjects(data)
+    }
+    fetchMyData()
+  }, [])
+
+  console.log(objects)
 
   return (
     <>
       <header></header>
       <main>
-        <div className='job'>
-          <img src="" alt="foto" />
+      {objects.map((obj, index) => (
+        <div className="job" key={index}>
+          <img src={obj.logo} alt="" />
           <div className="infoJob">
             <div className="info">
-              <p className="nameCompany">Photosnap</p>
-              <div className="office">Senior Frontend Developer</div>
+              <p className="nameCompany">{obj.company}</p>
+              <div className="office">{obj.position}</div>
               <div className="aboutJob">p</div>
             </div>
             <div className="requirements">
-              <div>FrontEnd</div>
+              <div>{obj.languages}</div>
             </div>
           </div>
         </div>
+        ))}
       </main>
+      
     </>
   )
 }
